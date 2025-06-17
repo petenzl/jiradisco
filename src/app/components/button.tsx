b36a7d9fb921ae9d4ab6e1ac87480a88132b6290
Alex Henderson <bittercoder@gmail.com>
2025-06-17 15:04:03 +1200

    Update button class to use cn for merging tailwind classes in a nicer way than before

--- src/app/components/button.tsx
+++ src/app/components/button.tsx
@@ -1,10 +1,12 @@
 import { ButtonHTMLAttributes, PropsWithChildren } from "react";
+import { cn } from "../lib/ts/cn";
 
 type ButtonProps = {
   variant: "link" | "button";
   onClick?: () => void;
   disabled?: boolean;
   type?: "button" | "submit" | "reset";
+  className?: string;
 } & Omit<ButtonHTMLAttributes<HTMLButtonElement>, "className" | "onClick" | "disabled" | "type">;
 
 export function Button({
@@ -13,6 +15,7 @@ export function Button({
   onClick,
   disabled = false,
   type = "button",
+  className,
   ...rest
 }: PropsWithChildren<ButtonProps>) {
   return (
@@ -20,11 +23,14 @@ export function Button({
       onClick={onClick}
       type={type}
       disabled={disabled}
-      className={`cursor-pointer transition-all duration-200 ${disabled ? "opacity-50 cursor-not-allowed" : ""} ${
-        variant === "link"
+      className={cn(
+        "cursor-pointer transition-all duration-200",
+        disabled && "opacity-50 cursor-not-allowed",
+        variant === "link" 
           ? "text-sm font-medium text-indigo-600 hover:text-indigo-500 underline-offset-2 hover:underline"
-          : "inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 active:translate-y-0.5 active:shadow-none"
-      }`}
+          : "inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 active:translate-y-0.5 active:shadow-none",
+        className
+      )}
       {...rest}
     >
       {children}
